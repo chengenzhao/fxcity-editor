9e87eb64b53ee944b7adf24721f5fc81a98507f5
chengenzhao <chengen.zhao@mail.mcgill.ca>
2024-12-22 10:46:32 +0800

    menu

--- src/main/java/com/whitewoodcity/fxcityeditor/GameApp.java
+++ src/main/java/com/whitewoodcity/fxcityeditor/GameApp.java
@@ -4,13 +4,19 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.scene.Cursor;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Screen;
 
 public class GameApp extends GameApplication {
+  final int HEIGHT = 1000;
+  final int WIDTH = (int) (Screen.getPrimary().getBounds().getWidth() / Screen.getPrimary().getBounds().getHeight() * 1000);
+
   @Override
   protected void initSettings(GameSettings settings) {
-    settings.setHeight(1000);
-    settings.setWidth((int) (Screen.getPrimary().getBounds().getWidth() / Screen.getPrimary().getBounds().getHeight() * 1000));
+    settings.setHeight(HEIGHT);
+    settings.setWidth(WIDTH);
     settings.setMainMenuEnabled(false);
     settings.setGameMenuEnabled(false);
   }
@@ -18,6 +24,16 @@ public class GameApp extends GameApplication {
   @Override
   protected void initUI() {
     FXGL.getGameScene().setCursor(Cursor.DEFAULT);
+
+    var menu = new Menu("File");
+
+    var loadImage = new MenuItem("Load Image");
+    menu.getItems().add(loadImage);
+
+    var menubar = new MenuBar(menu);
+    menubar.setPrefWidth(WIDTH);
+
+    FXGL.addUINode(menubar);
   }
 
 }
