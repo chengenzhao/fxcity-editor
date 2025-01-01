7485c792e6bbb8243930ad093019c547047487ca
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-01-01 14:09:49 +0800

    separate different textures

--- src/main/java/com/whitewoodcity/control/TransitTexture.java
+++ src/main/java/com/whitewoodcity/control/TransitTexture.java
@@ -1,23 +1,33 @@
 package com.whitewoodcity.control;
 
 import com.whitewoodcity.fxgl.texture.Texture;
+import javafx.scene.Group;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.transform.Rotate;
 
-public class TransitTexture extends Texture {
+public class TransitTexture extends Group {
 
   private final Rotate rotation = new Rotate();
+  private final ImageView imageView;
 
   public TransitTexture(Image image) {
-    super(image);
 
     this.getTransforms().add(rotation);
 
-    this.setFitWidth(image.getWidth());
-    this.setFitHeight(image.getHeight());
+    imageView = new ImageView(image);
+    imageView.setFitWidth(image.getWidth());
+    imageView.setFitHeight(image.getHeight());
+    this.getChildren().add(imageView);
+
+    this.getTransforms().add(rotation);
   }
 
   public Rotate getRotation() {
     return rotation;
+  }
+
+  public ImageView getImageView(){
+    return imageView;
   }
 }
--- src/main/java/com/whitewoodcity/fxcityeditor/GameApp.java
+++ src/main/java/com/whitewoodcity/fxcityeditor/GameApp.java
@@ -14,6 +14,7 @@ import com.whitewoodcity.model.View;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
@@ -95,135 +96,24 @@ public class GameApp extends GameApplication implements GameAppDecorator {
     var addViewComponentButton = new Button("+");
     addViewComponentButton.setOnAction(_ ->
       new ViewComponentDialog(fileBiMap.values()).showAndWait().ifPresent(view -> {
-        var image = new Image(view.image().toURI().toString());//file -> image
-        var texture = switch (view.textureType()){
-          case TRANSIT -> new TransitTexture(image);
-          default -> new AnimatedTexture(new AnimationChannel(image,1,Duration.seconds(1)));
-        };
+        var image = new Image(view.image().toURI().toString());
 
-        entity.getViewComponent().addChild(texture);
+        var name = view.image().getName();
+        name = name.substring(0, name.indexOf("."));
 
-        var rect = new Rectangle();
-        rect.widthProperty().bind(texture.fitWidthProperty());
-        rect.heightProperty().bind(texture.fitHeightProperty());
-        rect.xProperty().bindBidirectional(texture.xProperty());
-        rect.yProperty().bindBidirectional(texture.yProperty());
-        rect.setFill(Color.TRANSPARENT);
-        rect.setStroke(Color.web("#039ED3"));
-
-        var textureItem = new TreeItem<Node>();
-        var textureName = view.image().getName();
-        textureName = textureName.substring(0, textureName.indexOf("."));
-        var textureLabel = new Label(textureName);
-        var delTextureButton = new Button("×");
-        var textureHBox = new HBox(20, textureLabel, delTextureButton);
-        textureHBox.setAlignment(Pos.BASELINE_LEFT);
-        textureItem.setValue(textureHBox);
-
-        Circle temp = new Circle(6);
-        temp.setFill(Color.GREEN);
-        Rotate r = new Rotate();
-
-        Circle pivot = new Circle(7);
-        if(view.textureType() == View.TextureType.TRANSIT){
-          pivot.setFill(Color.web("#039ED3"));
-          var transitTexture = (TransitTexture)texture;
-          var rotation = transitTexture.getRotation();
-
-          rect.getTransforms().addAll(rotation,r);
-          transitTexture.getTransforms().add(r);
-          rotation.pivotXProperty().bind(pivot.centerXProperty());
-          rotation.pivotYProperty().bind(pivot.centerYProperty());
-
-          r.pivotXProperty().bind(XBindings.reduceDoubleValue(temp.centerXProperty(),temp.centerYProperty(), rotation.pivotXProperty(),rotation.pivotYProperty(), rotation.angleProperty(),
-            (x,y,x0,y0,a) -> Math.cos(a)*(x-x0) + Math.sin(a)*(y-y0)));//needs to apply new position of new coordinate
-          r.pivotYProperty().bind(XBindings.reduceDoubleValue(temp.centerXProperty(),temp.centerYProperty(), rotation.pivotXProperty(),rotation.pivotYProperty(), rotation.angleProperty(),
-            (x,y,x0,y0,a) -> -Math.sin(a)*(x-x0) + Math.cos(a)*(y-y0)));//needs to apply new position of new coordinate
-
-          temp.setOnMousePressed(originalE -> {
-            if(originalE.getButton()==MouseButton.SECONDARY){
-              r.setAngle(r.getAngle()+5);
-            }else{
-              var ox = originalE.getX();
-              var oy = originalE.getY();
-              var tx = temp.getCenterX();
-              var ty = temp.getCenterY();
-              temp.setOnMouseDragged(e -> {
-                double changeInX = e.getX() - ox;
-                double changeInY = e.getY() - oy;
-                temp.setCenterX(tx + changeInX);
-                temp.setCenterY(ty + changeInY);
-              });
-            }
-          });
-
-          pivot.setOnMousePressed(originalE -> {
-            if(originalE.getButton()==MouseButton.SECONDARY){
-              rotation.setAngle(rotation.getAngle()+5);
-              System.out.println(rotation.getPivotX());
-            }else{
-              selectTreeItem(textureHBox, treeview);
-              var ox = originalE.getX();
-              var oy = originalE.getY();
-              var tx = pivot.getCenterX();
-              var ty = pivot.getCenterY();
-              pivot.setOnMouseDragged(e -> {
-                double changeInX = e.getX() - ox;
-                double changeInY = e.getY() - oy;
-                pivot.setCenterX(tx + changeInX);
-                pivot.setCenterY(ty + changeInY);
-              });
-            }
-          });
+        switch (view.textureType()){
+          case TRANSIT -> {
+            var texture = new TransitTexture(image);
+            addTransitTexture(entityTree, name, texture, treeview, bottomPane, rightPane);
+          }
+          case ANIMATED -> {
+            var texture = new AnimatedTexture(new AnimationChannel(image,1,Duration.seconds(1)));
+            addAnimatedTexture(entityTree, name, texture, treeview, bottomPane, rightPane);
+          }
+          default -> {}
         }
-
-        textureHBox.setOnMousePressed(_ -> {
-          decorateBottomAndRightPane(texture, bottomPane, rightPane);
-          entity.getViewComponent().removeDevChild(rect);
-          entity.getViewComponent().addDevChild(rect);
-          if(view.textureType() == View.TextureType.TRANSIT){
-            entity.getViewComponent().removeDevChild(pivot);
-            entity.getViewComponent().addDevChild(pivot);
-
-            entity.getViewComponent().removeDevChild(temp);
-            entity.getViewComponent().addDevChild(temp);
-          }
-
-          rect.setOnMousePressed(originalE -> {
-            selectTreeItem(textureHBox, treeview);
-            var ox = originalE.getX();
-            var oy = originalE.getY();
-            var tx = rect.getX();
-            var ty = rect.getY();
-            rect.setOnMouseDragged(e -> {
-              double changeInX = e.getX() - ox;
-              double changeInY = e.getY() - oy;
-              rect.setX(tx + changeInX);
-              rect.setY(ty + changeInY);
-            });
-          });
-        });
-        texture.setOnMouseClicked(_ -> selectTreeItem(textureHBox, treeview));
-        rect.setOnMouseReleased(e -> {//deselect the view component
-          if (e.getButton() == MouseButton.SECONDARY)
-            freezeEvent(textureHBox);
-        });
-        textureHBox.setOnMouseReleased(e -> {//freeze event
-          if (e.getButton() == MouseButton.SECONDARY) {
-            entity.getViewComponent().removeDevChild(rect);
-            if(view.textureType() == View.TextureType.TRANSIT) entity.getViewComponent().removeDevChild(pivot);
-          }
-        });
-
-        delTextureButton.setOnAction(_ -> {
-          removeTreeItem(textureHBox,treeview);
-          entity.getViewComponent().removeChild(texture);
-        });
-
-        entityTree.getChildren().add(textureItem);
-        selectTreeItem(textureHBox, treeview);
-      })
-    );
+      }
+    ));
     entityHBox.setAlignment(Pos.BASELINE_LEFT);
     entityHBox.getChildren().addAll(new Label("Entity0"), addViewComponentButton);
     entityHBox.setOnMousePressed(_ -> decorateBottomAndRightPane(entity, bottomPane, rightPane));
@@ -266,5 +156,75 @@ public class GameApp extends GameApplication implements GameAppDecorator {
       treeView.getTreeItem(0).getChildren().add(treeItem);//row 0 is resources tree item
       selectTreeItem(label, treeView);
     }
+  }
+
+  private void addAnimatedTexture(TreeItem<Node> entityTree, String name, AnimatedTexture texture, TreeView<Node> treeview, Pane bottomPane, GridPane rightPane){
+
+    entity.getViewComponent().addChild(texture);
+
+    var rect = new Rectangle();
+    rect.widthProperty().bind(texture.fitWidthProperty());
+    rect.heightProperty().bind(texture.fitHeightProperty());
+    rect.xProperty().bindBidirectional(texture.xProperty());
+    rect.yProperty().bindBidirectional(texture.yProperty());
+    rect.setFill(Color.TRANSPARENT);
+    rect.setStroke(Color.web("#039ED3"));
+
+    var textureItem = new TreeItem<Node>();
+    var textureLabel = new Label(name);
+    var delTextureButton = new Button("×");
+    var textureHBox = new HBox(20, textureLabel, delTextureButton);
+    textureHBox.setAlignment(Pos.BASELINE_LEFT);
+    textureItem.setValue(textureHBox);
+
+    textureHBox.setOnMousePressed(_ -> {
+      decorateBottomAndRightPane(texture, bottomPane, rightPane);
+      entity.getViewComponent().removeDevChild(rect);
+      entity.getViewComponent().addDevChild(rect);
+
+      rect.setOnMousePressed(originalE -> {
+        selectTreeItem(textureHBox, treeview);
+        var ox = originalE.getX();
+        var oy = originalE.getY();
+        var tx = rect.getX();
+        var ty = rect.getY();
+        rect.setOnMouseDragged(e -> {
+          double changeInX = e.getX() - ox;
+          double changeInY = e.getY() - oy;
+          rect.setX(tx + changeInX);
+          rect.setY(ty + changeInY);
+        });
+      });
+    });
+    texture.setOnMouseClicked(_ -> selectTreeItem(textureHBox, treeview));
+    rect.setOnMouseReleased(e -> {//deselect the view component
+      if (e.getButton() == MouseButton.SECONDARY)
+        freezeEvent(textureHBox);
+    });
+    textureHBox.setOnMouseReleased(e -> {//freeze event
+      if (e.getButton() == MouseButton.SECONDARY) {
+        entity.getViewComponent().removeDevChild(rect);
+      }
+    });
+
+    delTextureButton.setOnAction(_ -> {
+      removeTreeItem(textureHBox,treeview);
+      entity.getViewComponent().removeChild(texture);
+    });
+
+    entityTree.getChildren().add(textureItem);
+    selectTreeItem(textureHBox, treeview);
+  }
+
+  private void addTransitTexture(TreeItem<Node> treeItem, String name, TransitTexture texture,TreeView<Node> treeView, Pane bottomPane, GridPane rightPane){
+    entity.getViewComponent().addChild(texture);
+
+    var rect = new Rectangle();
+    rect.widthProperty().bind(texture.getImageView().fitWidthProperty());
+    rect.heightProperty().bind(texture.getImageView().fitHeightProperty());
+    rect.xProperty().bindBidirectional(texture.translateXProperty());
+    rect.yProperty().bindBidirectional(texture.translateYProperty());
+    rect.setFill(Color.TRANSPARENT);
+    rect.setStroke(Color.web("#039ED3"));
   }
 }
