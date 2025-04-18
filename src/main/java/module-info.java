66f7c4c35156dc6bba260dad7e678fa0a09e729d
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-04-18 09:18:46 +0800

    fix bug

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,5 @@ open module com.whitewoodcity.fxcityeditor {
   requires com.almasb.fxgl.all;
   requires com.google.common;
   requires annotations;
-  requires org.checkerframework.checker.qual;
   requires com.fasterxml.jackson.databind;
 }
